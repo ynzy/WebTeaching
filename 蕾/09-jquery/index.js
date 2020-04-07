5cc4a93590a3d96c767de7450e16935b57dcb750
ynzy <758922096@qq.com>
2020-04-07 22:08:29 +0800

    解答问题

---
+++ 蕾/09-jquery/index.js
@@ -0,0 +1,14 @@
+var phone = '15930722692'
+phone = phone.substr(0, 3) + '****' + phone.substr(7)
+console.log(phone);
+
+
+//
+var typeName = {
+  '1': '充值打款',
+  '3': '充值打款',
+  '4': '上级扣款',
+  '6': '运费扣除'
+};
+console.log(typeName['1']);
+
