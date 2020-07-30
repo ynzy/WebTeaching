4a994d6a9374b9d109b50451720fd7ed7820dbf8
zhangyong <758922096@qq.com>
2020-07-30 18:14:15 +0800

    feat(model:all): 模板引擎

--- 其他/ajax/day01/app.js
+++ 其他/ajax/day01/app.js
@@ -9,6 +9,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.all('*', function (req, res, next) {
+	console.log(req.method);
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Headers', 'Content-type');
+	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
+	res.header('Access-Control-Max-Age', 1728000);//预请求缓存20天
+	next();
+});
+
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname, 'public')));
 
