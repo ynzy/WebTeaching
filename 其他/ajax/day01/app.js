0cf8d0d66d72705ad51d7367d49a9aafc6525421
zhangyong <758922096@qq.com>
2020-08-01 18:05:20 +0800

    feat(model:all): ajax扩展

--- 其他/ajax/day01/app.js
+++ 其他/ajax/day01/app.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(bodyParser.json());
 
 app.all('*', function (req, res, next) {
+	// 1.允许哪些客户端访问我
+	// * 代表允许所有的客户端访问我
+	// 注意：如果跨域请求中涉及到cookie信息传递，值不可以为*号 比如是具体的域名信息
+	// res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+	// 2.允许客户端使用哪些请求方法访问我
+	// res.header('Access-Control-Allow-Methods', 'get,post')
+	// 允许客户端发送跨域请求时携带cookie信息
+	// res.header('Access-Control-Allow-Credentials', true);
 	console.log(req.method);
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header('Access-Control-Allow-Headers', 'Content-type');
